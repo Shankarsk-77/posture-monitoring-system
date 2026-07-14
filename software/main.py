37b2914d59dca5efa4727df509f12e634348bda7
Shankarsk-77 <shankarskofficial@gmail.com>
2026-07-14 18:43:42 +0530

    Update main.py

--- software/main.py
+++ software/main.py
@@ -1,1 +1,23 @@
-print("AI-Powered Wearable Posture Monitoring and Correction System")
+import cv2
+import mediapipe as mp
+from posture_detector import PostureDetector
+
+cap = cv2.VideoCapture(0)
+
+detector = PostureDetector()
+
+while True:
+    success, frame = cap.read()
+
+    if not success:
+        break
+
+    frame = detector.detect(frame)
+
+    cv2.imshow("AI Posture Monitoring System", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
