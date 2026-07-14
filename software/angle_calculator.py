430bf103143886ce6a26af650f97b458b47fb875
Shankarsk-77 <shankarskofficial@gmail.com>
2026-07-14 18:46:31 +0530

    Create angle_calculator.py

---
+++ software/angle_calculator.py
@@ -0,0 +1,25 @@
+import math
+
+def calculate_angle(a, b, c):
+
+    angle = math.degrees(
+
+        math.atan2(
+            c[1]-b[1],
+            c[0]-b[0]
+        )
+
+        -
+
+        math.atan2(
+            a[1]-b[1],
+            a[0]-b[0]
+        )
+
+    )
+
+    if angle < 0:
+
+        angle += 360
+
+    return angle
